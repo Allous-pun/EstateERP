e30c7eaf9bacbe65b0b30e4db7e9085debc1ee99
Oula junior <oulapaulinda2004@gmail.com>
2026-03-17 15:00:25 +0300

    Add files via upload

--- userController.js
+++ userController.js
@@ -1,7 +1,3 @@
-const User = require('../models/userModel');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-
 exports.registerUser = async (req, res) => {
   try {
     const data = req.body;
@@ -10,44 +6,31 @@ exports.registerUser = async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     data.password = await bcrypt.hash(data.password, salt);
 
-    await User.createUser(data);
+    let result;
+
+    switch (data.role) {
+      case "ADMIN":
+        result = await User.createAdmin(data);
+        break;
+
+      case "TENANT":
+        result = await User.createTenant(data);
+        break;
+
+      case "GUARD":
+        result = await User.createGuard(data);
+        break;
+
+      default:
+        return res.status(400).json({ msg: "Invalid role" });
+    }
 
     res.status(201).json({
-      message: "User created successfully"
+      message: `${data.role} created successfully`,
+      result
     });
 
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
-};
-
-exports.loginUser = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    const user = await User.findByEmail(email);
-    if (!user) return res.status(400).json({ msg: "User not found" });
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
-
-    const token = jwt.sign(
-      { id: user.id, role: user.role },
-      "SECRET_KEY",
-      { expiresIn: "1d" }
-    );
-
-    res.json({
-      token,
-      user
-    });
-
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
-exports.getUsers = async (req, res) => {
-  const users = await User.getAllUsers();
-  res.json(users);
 };
